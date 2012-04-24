83cbf1bc5e08240e4340870e49df14bf09b71db1
J Tao <ljtao@seas.upenn.edu>
2012-04-24 17:38:54 -0400

    adding lessons to new lessons database

--- Trace2Learn/gen/edu/upenn/cis350/R.java
+++ Trace2Learn/gen/edu/upenn/cis350/R.java
@@ -18,35 +18,40 @@ public final class R {
     }
     public static final class id {
         public static final int ButtonLayout=0x7f050003;
-        public static final int back=0x7f050014;
+        public static final int back=0x7f050018;
         public static final int back2=0x7f050004;
-        public static final int back3=0x7f050011;
-        public static final int characters=0x7f05000c;
-        public static final int clear=0x7f050015;
+        public static final int back3=0x7f050015;
+        public static final int back4=0x7f05000d;
+        public static final int characters=0x7f05000f;
+        public static final int clear=0x7f050019;
         public static final int drawChar=0x7f050005;
         public static final int exitlesson=0x7f050009;
         public static final int icon=0x7f050000;
         public static final int label=0x7f050002;
-        public static final int lessons=0x7f05000e;
+        public static final int lesson_finish=0x7f05000c;
+        public static final int lessonbuilder=0x7f050011;
+        public static final int lessons=0x7f050012;
         public static final int lessonspace=0x7f050007;
+        public static final int lessonview=0x7f05000b;
         public static final int nextchar=0x7f05000a;
-        public static final int phrase_finish=0x7f050010;
-        public static final int phraseview=0x7f05000f;
+        public static final int phrase_finish=0x7f050014;
+        public static final int phraseview=0x7f050013;
         public static final int prevchar=0x7f050008;
-        public static final int save=0x7f050013;
+        public static final int save=0x7f050017;
         public static final int text1=0x7f050001;
-        public static final int touchpaint=0x7f050012;
-        public static final int trace=0x7f05000b;
+        public static final int touchpaint=0x7f050016;
+        public static final int trace=0x7f05000e;
         public static final int word=0x7f050006;
-        public static final int words=0x7f05000d;
+        public static final int words=0x7f050010;
     }
     public static final class layout {
         public static final int char_row=0x7f030000;
         public static final int characters=0x7f030001;
         public static final int learning=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int phrase=0x7f030004;
-        public static final int trace=0x7f030005;
+        public static final int lesson=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int phrase=0x7f030005;
+        public static final int trace=0x7f030006;
     }
     public static final class string {
         public static final int adder=0x7f04000b;
@@ -59,6 +64,8 @@ public final class R {
         public static final int formal_name=0x7f040002;
         public static final int hello=0x7f040000;
         public static final int lesson_button_title=0x7f040012;
+        public static final int lesson_finish=0x7f040014;
+        public static final int lessonbuilder_button_title=0x7f040013;
         public static final int nextchar=0x7f040010;
         public static final int phrase_finish=0x7f04000c;
         public static final int prevchar=0x7f04000f;
